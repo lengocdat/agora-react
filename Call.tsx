1df82b9974179742f274958f6d81b42cf7679b12
Dat Le <dat.le@executionlab.asia>
2021-06-08 10:55:11 +0700

    update

---
+++ Call.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import AgoraRTC from 'agora-rtc-sdk-ng';
+import useAgora from './hooks/useAgora';
+import MediaPlayer from './components/MediaPlayer';
+import './Call.css';
+
+const client = AgoraRTC.createClient({ codec: 'h264', mode: 'rtc' });
+function Call() {
+  const {
+    localAudioTrack, localVideoTrack, leave, join, joinState, remoteUsers
+  } = useAgora(client);
+  return (
+    <div className='call'>
+      <form className='call-form'>
+        <div className='button-group'>
+          <button id='join' type='button' className='btn btn-primary btn-sm' disabled={joinState} onClick={() => {join(process.env.APP_ID, process.env.CHANEL, process.env.TEMP_TOKEN)}}>Join</button>
+          <button id='leave' type='button' className='btn btn-primary btn-sm' onClick={() => {leave()}}>Leave</button>
+        </div>
+      </form>
+      <div className='player-container'>
+        <div className='local-player-wrapper'>
+          <p className='local-player-text'>{localVideoTrack && `localTrack`}{joinState && localVideoTrack ? `(${client.uid})` : ''}</p>
+          <MediaPlayer videoTrack={localVideoTrack} audioTrack={localAudioTrack}></MediaPlayer>
+        </div>
+        {remoteUsers.map(user => (<div className='remote-player-wrapper' key={user.uid}>
+            <p className='remote-player-text'>{`remoteVideo(${user.uid})`}</p>
+            <MediaPlayer videoTrack={user.videoTrack} audioTrack={user.audioTrack}></MediaPlayer>
+          </div>))}
+      </div>
+    </div>
+  );
+}
+
+export default Call;
---
+++ index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Call from './Call';
+
+ReactDOM.render(
+    <Call />,
+  document.getElementById('root')
+);
